6929e629c3cdbb9d56b5c19a2ab5fb325c3d62bd
NamNg <namduy1012@gmail.com>
2023-03-02 22:44:53 +0900

    Revise The Node

--- Blockchain-Node/node/src/chain_spec.rs
+++ Blockchain-Node/node/src/chain_spec.rs
@@ -92,6 +92,8 @@ pub fn local_testnet_config() -> Result<ChainSpec, String> {
 				vec![
 					authority_keys_from_seed("Alice"),
 					authority_keys_from_seed("Bob"),
+					authority_keys_from_seed("Eve"),
+					authority_keys_from_seed("Charlie"),
 				],
 				// Sudo account
 				get_account_id_from_seed::<sr25519::Public>("Alice"),
